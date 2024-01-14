043b8c3f23c6cf2857ee4484f3d75dc17048f3cd
sun <743685264@qq.com>
2024-01-14 19:00:43 +0800

    feat: 轮播图模块完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import type { BannerItem } from '@/types/home'
+import { getHomeBannerAPI } from '@/services/home';
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBanner = async () => {
+  const res: any = await getHomeBannerAPI()
+  console.log(res)
+  bannerList.value = res.result
+}
+
+// 在页面里，相当于 onMounted()
+onLoad(() => {
+  getHomeBanner()
+})
 </script>
 
 <template>
+  <!-- 导航栏 -->
   <CustomNavbar />
+  <!-- 轮播图 -->
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
-  <uni-card>
-    <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss">
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import { http } from '@/utils/https'
+import { http } from '@/utils/http'
 const memberStore = useMemberStore()
 // 测试请求
 const getData = async () => {
@@ -16,17 +16,12 @@ const getData = async () => {
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-          token: '12345',
-        })
-      "
-      size="mini"
-      plain
-      type="primary"
-    >
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋',
+        token: '12345',
+      })
+      " size="mini" plain type="primary">
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
@@ -37,3 +32,4 @@ const getData = async () => {
 <style lang="scss">
 //
 </style>
+@/utils/http
