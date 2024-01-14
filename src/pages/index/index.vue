de0a7051105d4501196bfcb4baa31b161f51396e
sun <743685264@qq.com>
2024-01-14 16:30:28 +0800

    feat: 安全区域适配

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <CustomNavbar />
   <view class="index">index</view>
   <uni-card>
     <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
